e2dc0e1f90f9581c3dd9e694c513d77592715b16
herrp <herrp@HERRPAULSLAPTOP>
2022-01-29 10:08:40 +0400

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using static Emgu.CV.CvInvoke;
+
+Console.Write("Input file name: ");
+var mat = Imread(Console.ReadLine());
+CvtColor(mat, mat, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+var img = mat.ToImage<Hsv, byte>();
+img[1] *= 1.5;
+CvtColor(img, img, Emgu.CV.CvEnum.ColorConversion.Hsv2Bgr);
+Imwrite("result.jpg", img);
+Console.WriteLine("Successful");
